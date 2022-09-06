8e47a14551d7245062e76c7e005124969185f78f
Daniel Ribas <danielribas1105@gmail.com>
2022-09-06 00:26:27 -0300

    update-220906

---
+++ PersonalVirtual/app/src/main/java/com/apps/personalvirtual/MembrosSuperiores.java
@@ -0,0 +1,30 @@
+package com.apps.personalvirtual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MembrosSuperiores extends AppCompatActivity {
+
+    private ListView exercSuper;
+    private String[] tiposSuper = {
+            "Peito","Costas","Ombros","Bíceps","Tríceps","Antebraço"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_membros_superiores);
+
+        exercSuper = findViewById(R.id.membrosSuperiores);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                tiposSuper
+        );
+        exercSuper.setAdapter(adapter);
+    }
+}
