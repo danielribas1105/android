19ee97150d191faf59d2cf60e1f3913d7fb43045
Daniel Ribas <danielribas1105@gmail.com>
2022-09-12 10:48:51 -0300

    update-220912

--- BancodeDadosSQLite/app/src/main/java/com/apps/bancodedadossqlite/MainActivity.java
+++ BancodeDadosSQLite/app/src/main/java/com/apps/bancodedadossqlite/MainActivity.java
@@ -2,7 +2,10 @@ package com.apps.bancodedadossqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            //Criar Banco de Dados
+            SQLiteDatabase bancoDados = openOrCreateDatabase("app",MODE_PRIVATE,null);
+            //Criar Tabelas
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (nome VARCHAR,idade INT(3))");
+            //Inserir Dados em uma Tabela
+            //bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Daniel',47)");
+            //bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Vitor',12)");
+            //bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Leticia',10)");
+            //Recuperar Dados
+            Cursor cursor = bancoDados.rawQuery("SELECT nome, idade FROM pessoas",null);
+            //Recuperando o indice da coluna
+            int indexNome = cursor.getColumnIndex("nome");
+            int indexIdade = cursor.getColumnIndex("idade");
+            cursor.moveToFirst();
+            while (cursor != null) {
+                Log.i("Resultado - Nome: ",cursor.getString(indexNome));
+                Log.i("Resultado - Idade: ",cursor.getString(indexIdade));
+                cursor.moveToNext();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
