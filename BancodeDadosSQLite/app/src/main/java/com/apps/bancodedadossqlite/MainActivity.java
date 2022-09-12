a2125682e2cba128a1fa7ade94b85b710e9e0301
Daniel Ribas <danielribas1105@gmail.com>
2022-09-12 19:18:13 -0300

    update-220912

--- BancodeDadosSQLite/app/src/main/java/com/apps/bancodedadossqlite/MainActivity.java
+++ BancodeDadosSQLite/app/src/main/java/com/apps/bancodedadossqlite/MainActivity.java
@@ -18,20 +18,26 @@ public class MainActivity extends AppCompatActivity {
             //Criar Banco de Dados
             SQLiteDatabase bancoDados = openOrCreateDatabase("app",MODE_PRIVATE,null);
             //Criar Tabelas
-            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (nome VARCHAR,idade INT(3))");
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    " nome VARCHAR,idade INT(3))");
+            //Excluir Tabelas
+            //bancoDados.execSQL("DROP TABLE pessoas");
             //Inserir Dados em uma Tabela
             //bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Daniel',47)");
             //bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Vitor',12)");
             //bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Leticia',10)");
             //Recuperar Dados
-            Cursor cursor = bancoDados.rawQuery("SELECT nome, idade FROM pessoas",null);
+            Cursor cursor = bancoDados.rawQuery("SELECT * FROM pessoas",null);
             //Recuperando o indice da coluna
+            int indexId = cursor.getColumnIndex("id");
             int indexNome = cursor.getColumnIndex("nome");
             int indexIdade = cursor.getColumnIndex("idade");
             cursor.moveToFirst();
             while (cursor != null) {
-                Log.i("Resultado - Nome: ",cursor.getString(indexNome));
-                Log.i("Resultado - Idade: ",cursor.getString(indexIdade));
+                String id = cursor.getString(indexId);
+                String nome = cursor.getString(indexNome);
+                String idade = cursor.getString(indexIdade);
+                Log.i("Resultado - Id: ",id+" / Nome: "+nome+" / Idade: "+idade);
                 cursor.moveToNext();
             }
 
