ce90bc5b0f5c27b9f6ff0fdddc1127a674669157
Daniel Ribas <danielribas1105@gmail.com>
2022-09-14 10:25:42 -0300

    update-220914

---
+++ FirebaseApp/app/src/main/java/com/apps/firebaseapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.apps.firebaseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.ktx.Firebase;
+
+import java.sql.DatabaseMetaData;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
