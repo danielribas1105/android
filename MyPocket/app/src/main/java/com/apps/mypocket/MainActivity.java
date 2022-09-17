032c7c075f973d031a63688978cb54c0a3c48991
Daniel Ribas <danielribas1105@gmail.com>
2022-09-17 00:42:10 -0300

    update-220917

--- MyPocket/app/src/main/java/com/apps/mypocket/MainActivity.java
+++ MyPocket/app/src/main/java/com/apps/mypocket/MainActivity.java
@@ -2,13 +2,23 @@ package com.apps.mypocket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.apps.mypocket.activity.CadastroActivity;
+import com.apps.mypocket.activity.LoginActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.app.SlideFragment;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
-public class MainActivity extends IntroActivity {
+public class MainActivity extends IntroActivity  {
+
+    //private Button btnCadastrar = findViewById(R.id.btnCadastrar);
+    //private TextView btnLogin = findViewById(R.id.textTenhoCadastro);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,7 @@ public class MainActivity extends IntroActivity {
                 .fragment(R.layout.fragment_slide_1)
                 .build()
         );
+
         addSlide(new FragmentSlide.Builder()
                 .background(R.color.white)
                 .fragment(R.layout.fragment_slide_2)
@@ -41,9 +52,33 @@ public class MainActivity extends IntroActivity {
         );
         addSlide(new FragmentSlide.Builder()
                 .background(R.color.white)
-                .fragment(R.layout.fragment_slide_5)
+                .fragment(R.layout.fragment_slide_cadastro)
                 .canGoForward(false)
                 .build()
         );
     }
+
+    /*
+    public void btnCadastrar (View view){
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CadastroActivity.class));
+            }
+        });
+    }
+
+    public void btnLogin(View view){
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+
+    }
+
+     */
+
 }
