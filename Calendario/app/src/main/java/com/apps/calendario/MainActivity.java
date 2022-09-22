7b0a5526fe4957ffa3a92cd4b41e44af39cc0dee
Daniel Ribas <danielribas1105@gmail.com>
2022-09-21 23:38:11 -0300

    update-220921

--- Calendario/app/src/main/java/com/apps/calendario/MainActivity.java
+++ Calendario/app/src/main/java/com/apps/calendario/MainActivity.java
@@ -11,21 +11,20 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private CalendarView calendarView;
+    private CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
         calendarView = findViewById(R.id.calendar);
+
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
                 Log.i("Data", "valor: "+ day + " " + month + " " + year);
             }
         });
-         */
     }
 }
