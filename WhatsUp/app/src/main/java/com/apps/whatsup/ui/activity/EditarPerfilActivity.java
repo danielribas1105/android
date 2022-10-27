75f5e638d3527d5bc88647211ceb03a228e80d02
Daniel Ribas <danielribas1105@gmail.com>
2022-10-27 15:10:12 -0300

    update-221027

--- WhatsUp/app/src/main/java/com/apps/whatsup/ui/activity/EditarPerfilActivity.java
+++ WhatsUp/app/src/main/java/com/apps/whatsup/ui/activity/EditarPerfilActivity.java
@@ -11,17 +11,23 @@ import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.apps.whatsup.R;
 import com.apps.whatsup.helper.Consent;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class EditarPerfilActivity extends AppCompatActivity {
 
     private ImageButton imgCamera, imgGallery;
+    private CircleImageView circleImageView;
     private static final int SELECT_CAMERA = 100;
     private static final int SELECT_GALLERY = 200;
 
@@ -43,6 +49,7 @@ public class EditarPerfilActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        circleImageView = findViewById(R.id.imgCirclePerfil);
         imgCamera = findViewById(R.id.imgBtnCamera);
         imgGallery = findViewById(R.id.imgBtnGaleria);
 
@@ -52,6 +59,7 @@ public class EditarPerfilActivity extends AppCompatActivity {
                 Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if(i.resolveActivity(getPackageManager()) != null){
                     startActivityForResult(i,SELECT_CAMERA);
+
                 }
             }
         });
@@ -60,6 +68,7 @@ public class EditarPerfilActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
                 if(i.resolveActivity(getPackageManager()) != null){
                     startActivityForResult(i,SELECT_GALLERY);
                 }
@@ -71,6 +80,34 @@ public class EditarPerfilActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            Bitmap imagem = null;
+
+            try{
+                switch (requestCode){
+                    case SELECT_CAMERA:
+                        imagem = (Bitmap) data.getExtras().get("dados");
+                        break;
+                    case SELECT_GALLERY:
+                        Uri localImgSelected = data.getData();
+                        imagem = MediaStore.Images.Media
+                                .getBitmap(getContentResolver(),localImgSelected);
+                        break;
+                }
+                //Configurar a imagem recebida
+                if(imagem != null){
+                    circleImageView.setImageBitmap(imagem);
+                }else{
+                    Toast.makeText(this,"Imagem vazia", Toast.LENGTH_SHORT).show();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }else{
+            //mensagem de erro
+        }
+
     }
 
     @Override
