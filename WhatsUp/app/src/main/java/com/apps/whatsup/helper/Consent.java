f4fa113960d0dd0092e592c1172228fc43bdaf12
Daniel Ribas <danielribas1105@gmail.com>
2022-10-26 23:52:02 -0300

    update-221026

--- WhatsUp/app/src/main/java/com/apps/whatsup/helper/Consent.java
+++ WhatsUp/app/src/main/java/com/apps/whatsup/helper/Consent.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
@@ -11,7 +12,7 @@ import java.util.List;
 
 public class Consent {
 
-    public static boolean validateConsent(String[] consents, Activity activity){
+    public static boolean validateConsent(String[] consents, Activity activity, int requestCode){
 
         if(Build.VERSION.SDK_INT >= 23){
             List<String> listConsent = new ArrayList<>();
@@ -19,12 +20,21 @@ public class Consent {
             /*Percorrer as permissões necessárias,
             * verificando uma a uma se já estão liberadas*/
             for(String consent: consents){
-                Boolean checkConsent = ContextCompat.checkSelfPermission(activity,consent) == PackageManager.PERMISSION_GRANTED;
+                Boolean checkConsent = ContextCompat.checkSelfPermission(activity,consent) ==
+                        PackageManager.PERMISSION_GRANTED;
                 if(!checkConsent) listConsent.add(consent);
             }
+            /*Caso a lista esteja vazia,
+            não precisa solicitar permissão*/
+            if(listConsent.isEmpty()) return true;
+
+            String[] newConsent = new String[listConsent.size()];
+            listConsent.toArray(newConsent);
+
+            //Solicitar permissões
+            ActivityCompat.requestPermissions(activity,newConsent,requestCode);
 
         }
-
         return true;
     }
 }
--- WhatsUp/app/src/main/java/com/apps/whatsup/ui/activity/EditarPerfilActivity.java
+++ WhatsUp/app/src/main/java/com/apps/whatsup/ui/activity/EditarPerfilActivity.java
@@ -1,9 +1,14 @@
 package com.apps.whatsup.ui.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.apps.whatsup.R;
@@ -22,12 +27,38 @@ public class EditarPerfilActivity extends AppCompatActivity {
         setContentView(R.layout.activity_editar_perfil);
 
         //Validar permissões
-        Consent.validateConsent(consent, this);
+        Consent.validateConsent(consent, this, 1);
 
         Toolbar toolbar = findViewById(R.id.toolbarHome);
         toolbar.setTitle("Editar");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for(int consentResult: grantResults){
+            if(consentResult == PackageManager.PERMISSION_DENIED){
+                alertValidateConsent();
+            }
+        }
+    }
+
+    public void alertValidateConsent(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões Negadas");
+        builder.setMessage("Para utilizar o App é necessário aceitar as permissões!");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
