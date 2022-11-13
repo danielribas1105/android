68f989f648081b2708506f1b0b02877d687a5227
Daniel Ribas <danielribas1105@gmail.com>
2022-11-13 20:29:34 -0300

    update-221113

---
+++ DRPersonalManager/app/src/main/java/com/apps/drpersonalmanager/ui/activity/EditTrainigActivity.java
@@ -0,0 +1,16 @@
+package com.apps.drpersonalmanager.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.apps.drpersonalmanager.R;
+
+public class EditTrainigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_trainig);
+    }
+}
